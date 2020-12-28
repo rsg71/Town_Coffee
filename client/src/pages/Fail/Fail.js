22c901df35a8de1026bc22765582c20e0fb7e8bb
James Lamb <jameslamb13@gmail.com>
2020-12-28 00:42:08 -0500

    added a little flair to fail page.

--- client/src/pages/Fail/Fail.js
+++ client/src/pages/Fail/Fail.js
@@ -1,21 +1,29 @@
 import React from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
+import "./style.css";
 
 function Fail () {
     return (
         <> 
+        <br/>
+            <br/>
             <Container>
-                <Row>
+                <Row className="failed">
                     <Col>
+                    
                     <h1>Failed to Subscribe</h1>
                     
-                    <h2> :( </h2>
+                    <h2><i className="far fa-sad-tear"></i></h2>
 
                     <a href="/subscriptions"><Button>Go Back</Button></a>
                     
                     </Col>
                 </Row>
             </Container>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
         </>
     )
 }
