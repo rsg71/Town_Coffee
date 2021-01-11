39417f36a465f4c59044f58cf3f8f5cd9353599b
Robert Greenawalt <robert.greenawalt29@gmail.com>
2021-01-10 19:09:17 -0500

    added spacing to put footer at bottom of page

--- client/src/pages/CanceledPayment/CanceledPayment.js
+++ client/src/pages/CanceledPayment/CanceledPayment.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import { Container, Row, Col } from "react-bootstrap"
 import { Link } from "react-router-dom"
+import "./CanceledPayment.css"
 
 export default function CanceledPayment() {
     return (
         <>
-            <Container>
+            <Container id="canceledPaymentContainer">
                 <Row>
-                    <Col>
+                    <Col id="canceledPaymentCol">
                         <h1>Your payment has been cancelled</h1>
                         <br />
                         <Link to="/coffee">Click here to continue shopping</Link>
+
+                        
+
                     </Col>
                 </Row>
             </Container>
